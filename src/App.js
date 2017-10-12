d16d618b153788b0df7a889af14abef12774f0ac
Owais Ahmed <owaisahmed@HoadajMes-MacBook-Pro.local>
2017-10-12 21:46:13 +0500

    router V4

--- src/App.js
+++ src/App.js
@@ -1,73 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import axios from 'axios';
-import Map from './map';
-import './App.css';
+import Home from  './components/home'
+import Routes from './routes'
 
-class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state={
-      events: []
-      }
-
-    this.sendrequest=this.sendrequest.bind(this)
-  }
-
-HandleChange=(event)=>{
-  
-  this.setState({
-  artistname:event.target.value
-})
-
-}
-
-sendrequest=()=>{
-  axios.get("http://api.songkick.com/api/3.0/search/artists.json?query=" + this.state.artistname + "&apikey=M0d1Hijfu5f8q4cc")
-    .then(response => {
-           console.log(response.data.resultsPage.results.artist[0].id);
-           //this.setState({artistid:response.data.resultsPage.results.artist[0].id})
-           axios.get("http://api.songkick.com/api/3.0/artists/" + response.data.resultsPage.results.artist[0].id + "/calendar.json?apikey=M0d1Hijfu5f8q4cc")
-            .then(response => {
-                   console.log(response.data.resultsPage.results.event);
-                   this.setState({events:response.data.resultsPage.results.event})
-                  })
-                  .catch((error) => {
-                    console.log('error ' + error);
-                  });
-                  })
-          .catch((error) => {
-            console.log('error ' + error);
-          });
-
-}
-  
-
+class componentName extends Component {
   render() {
-      const city = this.state.events.map((e,i)=>{
-      return (<li key={i}>{e.location.city + " --- " + e.start.date + " ---" + e.venue.displayName}</li>)
-     })
-      const startdate = this.state.events.map((e)=>{
-      console.log(e.start.date)
-     })
-    console.log(this.state.events);
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Here is what trending on MuBang ♫ </h2>
-        </div>
-        <p className="App-intro">
-        <input type="text" name="artistname" onChange={this.HandleChange}/>
-        <button onClick={this.sendrequest}>Search box</button>
-        </p>
-        <ul>{city}</ul>
-        <Map/>
-
+      <div>
+        <Routes/>
       </div>
     );
   }
 }
 
-export default App;
+export default componentName;
---
+++ src/components/home.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import axios from 'axios';
+//import Map from './map';
+import '../App.css';
+
+import {
+  Link
+} from 'react-router-dom'
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state={
+      events: []
+      }
+
+    this.sendrequest=this.sendrequest.bind(this)
+  }
+
+HandleChange=(event)=>{
+  
+  this.setState({
+  artistname:event.target.value
+})
+
+}
+
+sendrequest=()=>{
+  axios.get("http://api.songkick.com/api/3.0/search/artists.json?query=" + this.state.artistname + "&apikey=M0d1Hijfu5f8q4cc")
+    .then(response => {
+           console.log(response.data.resultsPage.results.artist[0].id);
+           //this.setState({artistid:response.data.resultsPage.results.artist[0].id})
+           axios.get("http://api.songkick.com/api/3.0/artists/" + response.data.resultsPage.results.artist[0].id + "/calendar.json?apikey=M0d1Hijfu5f8q4cc")
+            .then(response => {
+                   console.log(response.data.resultsPage.results.event);
+                   this.setState({events:response.data.resultsPage.results.event})
+                  })
+                  .catch((error) => {
+                    console.log('error ' + error);
+                  });
+                  })
+          .catch((error) => {
+            console.log('error ' + error);
+          });
+
+}
+  
+
+  render() {
+      const city = this.state.events.map((e,i)=>{
+      return (<div key={i}><button >{e.location.city + " --- " + e.start.date + " ---" + e.venue.displayName}</button></div>)
+     })
+      const startdate = this.state.events.map((e)=>{
+      console.log(e.start.date)
+     })
+    console.log(this.state.events);
+    return (
+      <div>
+        {this.props.routes}
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Here is what trending on MuBang ♫ </h2>
+        </div>
+        <p className="App-intro">
+        <input type="text" name="artistname" onChange={this.HandleChange}/>
+        <button onClick={this.sendrequest}>Search box</button>
+        <li><Link to='/about'>about</Link></li>
+        </p>
+      
+      </div>
+      {city}
+
+      
+      </div>
+    );
+  }
+}
+
+export default App;
